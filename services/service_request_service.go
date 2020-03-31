3d77a61bda817065ced5d963240e88f699ba5bce
Tejash <tejashl@sahajsoft.com>
2020-03-31 12:50:58 +0530

    added test cases for service request service

--- services/service_request_service.go
+++ services/service_request_service.go
@@ -3,31 +3,47 @@ package services
 import (
 	"clamp-core/models"
 	"clamp-core/repository"
-	"fmt"
 )
 
-//FindByID is
-func FindByID(serviceReq models.ServiceRequest) {
+type serviceRequestRepo interface {
+	selectQuery(interface{}) error
+	insertQuery(interface{}) error
+}
+
+type serviceRequestRepoImpl struct {
+}
+
+func (s serviceRequestRepoImpl) insertQuery(model interface{}) error {
 	db := repository.GetDB()
+	return db.Insert(model)
+}
 
+func (s serviceRequestRepoImpl) selectQuery(model interface{}) error {
+	db := repository.GetDB()
+	return db.Select(model)
+}
+
+var repo serviceRequestRepo
+
+func init() {
+	repo = serviceRequestRepoImpl{}
+}
+
+//FindServiceRequestByID is
+func FindServiceRequestByID(serviceReq *models.ServiceRequest) {
 	pgServReq := serviceReq.ToPgServiceRequest()
-	err := db.Select(&pgServReq)
-
-	fmt.Println(pgServReq)
-
+	err := repo.selectQuery(&pgServReq)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func SaveServiceRequest(serviceReq models.ServiceRequest) models.ServiceRequest {
-	db := repository.GetDB()
-
+func SaveServiceRequest(serviceReq models.ServiceRequest) (models.ServiceRequest, error) {
 	pgServReq := serviceReq.ToPgServiceRequest()
-	err := db.Insert(&pgServReq)
+	err := repo.insertQuery(&pgServReq)
 
 	if err != nil {
 		panic(err)
 	}
-	return serviceReq
+	return serviceReq, err
 }
