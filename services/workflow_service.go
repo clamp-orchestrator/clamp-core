975e7b7aba3809ede2e2516c0c37bd79d1ca9a2f
Tejash <tejashl@sahajsoft.com>
2020-03-31 12:55:04 +0530

    removed commented code which was made by mistake

--- services/workflow_service.go
+++ services/workflow_service.go
@@ -2,18 +2,20 @@ package services
 
 import (
 	"clamp-core/models"
+	"clamp-core/repository"
+	"fmt"
 )
 
 func SaveServiceFlow(serviceFlowReg models.Workflow) models.Workflow {
-	//db := repository.GetDB()
-	//
-	//pgServReq := serviceFlowReg.ToPGWorkflow()
-	//err := db.Insert(&pgServReq)
+	db := repository.GetDB()
 
-	//fmt.Println(pgServReq)
-	//
-	//if err != nil {
-	//	panic(err)
-	//}
+	pgServReq := serviceFlowReg.ToPGWorkflow()
+	err := db.Insert(&pgServReq)
+
+	fmt.Println(pgServReq)
+
+	if err != nil {
+		panic(err)
+	}
 	return serviceFlowReg
 }
