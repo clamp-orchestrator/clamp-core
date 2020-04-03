cd5fdf34838e46a1d6de23de07b54363f4eee753
Lohith KM <lohithkm@sahajsoft.com>
2020-04-03 14:27:23 +0530

    Storing step level data in the step status table

---
+++ services/step_status_service.go
@@ -0,0 +1,29 @@
+package services
+
+import (
+	"clamp-core/models"
+	"fmt"
+	"github.com/google/uuid"
+)
+
+//FindServiceRequestByID is
+func FindServiceRequestByID(serviceRequestId uuid.UUID) (*models.ServiceRequest, error) {
+	serviceRequestReq := models.ServiceRequest{ID: serviceRequestId}
+	fmt.Println("Service Request request is -- ", serviceRequestReq)
+	serviceReq := new(models.ServiceRequest)
+	err := repo.whereQuery(serviceReq, "service_request.id = ?", serviceRequestId)
+	if err != nil {
+		fmt.Errorf("No record found with given service request id %s", serviceRequestId)
+	}
+	return serviceReq, err
+}
+
+func SaveServiceRequest(serviceReq models.ServiceRequest) (models.ServiceRequest, error) {
+	pgServReq := serviceReq.ToPgServiceRequest()
+	err := repo.insertQuery(&pgServReq)
+
+	if err != nil {
+		panic(err)
+	}
+	return serviceReq, err
+}
