c49a21e6effb43bddd20e97fb1df0544ada93009
Tejash <tejashl@sahajsoft.com>
2020-04-17 17:33:27 +0530

    added http api to receive step response

---
+++ handlers/step_response_handler.go
@@ -0,0 +1,26 @@
+package handlers
+
+import (
+	"clamp-core/models"
+	"clamp-core/services"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func createStepResponseHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var request models.AsyncResumeStepExecutionRequest
+		err := c.ShouldBindJSON(&request)
+		if err != nil {
+			errorResponse := models.CreateErrorResponse(http.StatusBadRequest, err.Error())
+			log.Println(err)
+			c.JSON(http.StatusBadRequest, errorResponse)
+			return
+		}
+		log.Printf("[HTTP Consumer] : Received step completed response: %v", request)
+		log.Printf("[HTTP Consumer] : Pushing step completed response to channel")
+		services.AddAsyncResumeStepExecutionRequestToChannel(request)
+		c.JSON(http.StatusOK, models.CreateSuccessResponse(http.StatusOK, "success"))
+	}
+}
