003214c177674f2a5dbb25979dea63dd8c27a869
Lohith KM <lohithkm@sahajsoft.com>
2020-07-02 18:37:00 +0530

    Added tests and updated rabbitmq details in test files and postman collection

--- handlers/service_request_handler_test.go
+++ handlers/service_request_handler_test.go
@@ -115,7 +115,7 @@ func CreateWorkflowIfItsAlreadyDoesNotExists() {
 		Enabled:   false,
 		Val: &executors.HttpVal{
 			Method:  "POST",
-			Url:     "http://18.236.212.57:3333/api/v1/login",
+			Url:     "http://54.70.48.149:3333/api/v1/login",
 			Headers: "",
 		},
 	}
