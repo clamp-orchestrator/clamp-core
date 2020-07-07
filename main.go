65bc7ef7d2e9c12f8590fd6b772a370e2a644c1f
Lohith KM <lohithkm@sahajsoft.com>
2020-07-07 15:32:55 +0530

    Added PORT configuration

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"clamp-core/config"
 	"clamp-core/handlers"
 	"clamp-core/listeners"
 	"clamp-core/migrations"
 	"log"
+	"os"
 )
 
 func main() {
 	//defer repository.CloseDB()
+	os.Setenv("PORT", config.ENV.PORT)
 	migrations.Migrate()
 	listeners.StepResponseListener.Listen()
 	handlers.LoadHTTPRoutes()
