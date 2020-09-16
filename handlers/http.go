c526e6a72970132ef8f4342b0daf0c23f7f74086
Tejash <tejashl@sahaj.ai>
2020-09-16 10:48:28 +0530

    added initial cors configuration

--- handlers/http.go
+++ handlers/http.go
@@ -26,7 +26,24 @@ func setupRouter() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.GET("/workflows", getWorkflows())
 	r.GET("/serviceRequests/:workflowName", findServiceRequestByWorkflowNameHandler())
+	r.Use(CORSMiddleware())
 	return r
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
 
 //LoadHTTPRoutes loads all HTTP api routes
