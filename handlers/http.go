c667ca889c8b7fb082d9f2f1017eadcd35043a00
Tejash <tejash.jl1020@gmail.com>
2020-09-16 16:25:20 +0530

    added cors plugin

--- handlers/http.go
+++ handlers/http.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	"github.com/swaggo/gin-swagger"
@@ -26,24 +27,8 @@ func setupRouter() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.GET("/workflows", getWorkflows())
 	r.GET("/serviceRequests/:workflowName", findServiceRequestByWorkflowNameHandler())
-	r.Use(CORSMiddleware())
+	r.Use(cors.Default())
 	return r
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
 
 //LoadHTTPRoutes loads all HTTP api routes
