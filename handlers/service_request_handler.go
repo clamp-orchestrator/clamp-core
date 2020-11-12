c693b61fc1acf146ef9e752e7f508972d6d38362
Tejash <tejash.jl1020@gmail.com>
2020-11-12 10:50:39 +0530

    moved hard coded values to constants

--- handlers/service_request_handler.go
+++ handlers/service_request_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"clamp-core/models"
 	"clamp-core/services"
+	"clamp-core/utils"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
@@ -165,7 +166,7 @@ func findServiceRequestByWorkflowNameHandler() gin.HandlerFunc {
 		}
 		pageNumber, pageNumberErr := strconv.Atoi(pageNumberStr)
 		pageSize, pageSizeErr := strconv.Atoi(pageSizeStr)
-		if pageNumberErr != nil || pageSizeErr != nil || pageSize < 1 || pageNumber < 1 {
+		if pageNumberErr != nil || pageSizeErr != nil || pageSize < utils.MinPageSize || pageNumber < utils.MinPageNumber {
 			err := errors.New("page number or page size is not in proper format")
 			prepareErrorResponse(err, c)
 			return
--- utils/constants.go
+++ utils/constants.go
@@ -9,4 +9,7 @@ const ServiceRequestWorkersSize = 100
 const ResumeStepResponseChannelSize = 1000
 const ResumeStepResponseWorkersSize = 100
 
+const MinPageSize = 1
+const MinPageNumber = 1
+
 var MilliSecondsDivisor int64 = 1000000
