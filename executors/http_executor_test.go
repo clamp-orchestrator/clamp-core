8262cdfb14d7e66ebb4922750b3a3e2904d7e5bb
Sivachandran Paramasivam <sivachandran.p@gmail.com>
2021-03-23 13:29:39 +0530

    Replace mocky.io usage with httptest server

--- executors/http_executor_test.go
+++ executors/http_executor_test.go
@@ -2,64 +2,72 @@ package executors
 
 import (
 	"encoding/json"
-	"reflect"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestHttpVal_DoExecute(t *testing.T) {
+	assert := assert.New(t)
+
+	httpRequestBody := map[string]interface{}{"k": "v"}
+	httpResponseBody := map[string]interface{}{"id": "1234", "name": "ABC", "email": "abc@sahaj.com", "org": "sahaj"}
+
+	var requestBody interface{}
+	var requestHeader http.Header
+
+	httpTestServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			w.WriteHeader(http.StatusNotFound)
+			io.WriteString(w, "Not found")
+			return
+		}
+
+		json.NewDecoder(r.Body).Decode(&requestBody)
+		requestHeader = r.Header
+
+		json.NewEncoder(w).Encode(httpResponseBody)
+	}))
+
+	defer httpTestServer.Close()
+
 	type args struct {
 		requestBody interface{}
 	}
 	tests := []struct {
-		name       string
-		fields     HTTPVal
-		args       args
-		want       interface{}
-		wantErr    bool
-		wantErrMsg string
+		name              string
+		fields            HTTPVal
+		args              args
+		wantRequestBody   interface{}
+		wantRequestHeader http.Header
+		wantResponseBody  interface{}
+		wantErrMsg        string
 	}{
 		{
 			name: "TestShouldExecuteHTTPStep",
 			fields: HTTPVal{
 				Method:  "GET",
-				URL:     "https://run.mocky.io/v3/0590fbf8-0f1c-401c-b9df-65e98ef0385d",
-				Headers: "",
+				URL:     httpTestServer.URL,
+				Headers: "Content-Type:application/json;X-Token:abc",
 			},
 			args: args{
-				requestBody: map[string]interface{}{"k": "v"},
+				requestBody: httpRequestBody,
 			},
-			want:       map[string]interface{}{"id": "1234", "name": "ABC", "email": "abc@sahaj.com", "org": "sahaj"},
-			wantErr:    false,
-			wantErrMsg: "",
-		},
-		{
-			name: "TestShouldExecuteHTTPStepWithHeaders",
-			fields: HTTPVal{
-				Method:  "GET",
-				URL:     "https://run.mocky.io/v3/0590fbf8-0f1c-401c-b9df-65e98ef0385d",
-				Headers: "Content-Type:application/json;token:abc",
-			},
-			args: args{
-				requestBody: map[string]interface{}{"k": "v"},
-			},
-			want:       map[string]interface{}{"id": "1234", "name": "ABC", "email": "abc@sahaj.com", "org": "sahaj"},
-			wantErr:    false,
-			wantErrMsg: "",
+			wantRequestBody:   httpRequestBody,
+			wantRequestHeader: http.Header{"Content-Type": {"application/json"}, "X-Token": {"abc"}},
+			wantResponseBody:  map[string]interface{}{"id": "1234", "name": "ABC", "email": "abc@sahaj.com", "org": "sahaj"},
+			wantErrMsg:        "",
 		},
 		{
 			name: "TestShouldThrowErrorWhileExecutingStep",
 			fields: HTTPVal{
 				Method:  "GET",
-				URL:     "https://run.mocky.io/v3/nonexistent",
+				URL:     httpTestServer.URL + "/non_existent_path",
 				Headers: "",
 			},
-			args: args{
-				requestBody: map[string]interface{}{"k": "v"},
-			},
-			want:       nil,
-			wantErr:    true,
 			wantErrMsg: "Not found",
 		},
 		{
@@ -69,11 +77,6 @@ func TestHttpVal_DoExecute(t *testing.T) {
 				URL:     "http://localhost:3333/api/v1/user",
 				Headers: "",
 			},
-			args: args{
-				requestBody: map[string]interface{}{"k": "v"},
-			},
-			want:       map[string]interface{}{"id": "1234", "name": "ABC", "email": "abc@sahaj.com", "org": "sahaj"},
-			wantErr:    true,
 			wantErrMsg: "connect: connection refused",
 		},
 	}
@@ -84,20 +87,33 @@ func TestHttpVal_DoExecute(t *testing.T) {
 				URL:     tt.fields.URL,
 				Headers: tt.fields.Headers,
 			}
+
 			got, err := httpVal.DoExecute(tt.args.requestBody, "")
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DoExecute() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if err != nil {
-				assert.Contains(t, err.Error(), tt.wantErrMsg)
+			if tt.wantErrMsg != "" {
+				assert.Contains(err.Error(), tt.wantErrMsg)
 			} else {
-				var responsePayload map[string]interface{}
-				err = json.Unmarshal([]byte(got.(string)), &responsePayload)
+				assert.NoError(err)
+
+				var responseBody map[string]interface{}
+				err = json.Unmarshal([]byte(got.(string)), &responseBody)
 				if err != nil {
 					t.Errorf("DoExecute() error = %v", err)
-				} else if !reflect.DeepEqual(responsePayload, tt.want) {
-					t.Errorf("DoExecute() got = %v, want %v", got, tt.want)
+				}
+
+				if tt.wantRequestBody != nil {
+					assert.Equal(tt.wantRequestBody, requestBody)
+				}
+
+				if tt.wantRequestHeader != nil {
+					for k, v := range tt.wantRequestHeader {
+						v2, ok := requestHeader[k]
+						assert.True(ok)
+						assert.Equal(v, v2)
+					}
+				}
+
+				if tt.wantResponseBody != nil {
+					assert.Equal(tt.wantResponseBody, responseBody)
 				}
 			}
 		})
